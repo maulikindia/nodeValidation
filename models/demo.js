f7a2cf3b302f822ba45eafc1c438eac1d16a1fab
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-07 13:02:58 +0530

    demo model changes

--- models/demo.js
+++ models/demo.js
@@ -1,10 +1,10 @@
 let mongoose = require('mongoose');
 
 let newSchema = new mongoose.Schema({
-
     name: { type: String },
     rollNo: { type: String },
-    email: { type: String }
+    email: { type: String },
+    role: { type: String, enum: ['student', 'monitor'] }
 });
 
 module.exports = mongoose.model('demo', newSchema);
