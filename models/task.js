859eff56d69f561cb9818501db6eb772b8a09dd4
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-07 17:35:31 +0530

    task model added and get ,post api added

---
+++ models/task.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+
+let taskSchema = new mongoose.Schema({
+
+    taskId: { type: Number },
+    desc: { type: String }
+
+});
+
+
+taskSchema.statics.findByTaskId = function myFunc(id) {
+    // return this.find({ taskId: id });
+    console.log('hhh')
+}
+
+
+module.exports = mongoose.model('task', taskSchema);
+// let tsk = mongoose.model('task', taskSchema);
+
+module.exports = tsk;
